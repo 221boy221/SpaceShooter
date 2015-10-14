0972581da34bc6b6e367e067b07bc55063ae2392
221boy221 <warrock.boy@hotmail.com>
2015-10-15 00:50:18 +0200

    Cleaned up code

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,11 +5,9 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float _mouseSensitivity;
-    float _verticleMouseRange = 60.0f;
-    float _maxSpeed = 10.0f;
-    [SerializeField] float _movementSpeed = 100.0f;
-    [SerializeField] float _rotationSpeed = 50.0f;
+    float _mouseSensitivity = 2.0f;
+    float _movementSpeed = 300.0f;
+    float _rotationSpeed = 10.0f;
     Vector2 _velocity = new Vector2(0,0);
     Rigidbody _rigidbody;
 
@@ -23,27 +21,22 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Rotation();
+        MouseRotation();
         Movement();
     }
 
-    void Rotation()
+    void MouseRotation()
     {
-        Debug.Log("Rot");
         float horizontalRot = 0;
         float verticleRot = 0;
-        Debug.Log(horizontalRot);
-        Debug.Log(verticleRot);
         horizontalRot = Input.GetAxisRaw("Mouse X") * _mouseSensitivity;
         verticleRot -= Input.GetAxisRaw("Mouse Y") * (_mouseSensitivity / 2);
 
         transform.Rotate(verticleRot, horizontalRot, 0);
-        //Camera.main.transform.localRotation = Quaternion.Euler(verticleRot, 0, 0);
     }
 
     void Movement()
     {
-        //Debug.Log("Velocity: " + _rigidbody.velocity);
         _rigidbody.AddForce(Input.GetAxis("Vertical") * transform.forward * Time.deltaTime * _movementSpeed);
         _rigidbody.AddForce(Input.GetAxis("Horizontal") * transform.right * Time.deltaTime * _movementSpeed);
         _rigidbody.AddTorque(transform.forward * Input.GetAxis("Rotation") * Time.deltaTime * _rotationSpeed);
